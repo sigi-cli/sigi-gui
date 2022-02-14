cb3f066a2611197a40fb10bf7ae6465da213fad3
♪ hiljusti 🎮 <hiljusti@pm.me>
2022-02-13 16:10:26 -0800

    Abstract away some gross stuff... kinda

--- src/gui.rs
+++ src/gui.rs
@@ -1,8 +1,8 @@
-use eframe::epi;
 use eframe::egui::*;
-use sigi::{output::*, effects::*};
+use eframe::epi;
 use gag::BufferRedirect;
-use std::io::{BufReader};
+use sigi::{effects::*, output::*};
+use std::io::BufReader;
 
 pub const RELEASE_VERSION: &'static str = std::env!("CARGO_PKG_VERSION");
 
@@ -17,31 +17,46 @@ impl epi::App for SigiApp {
     fn update(&mut self, ctx: &CtxRef, _frame: &epi::Frame) {
         CentralPanel::default().show(ctx, |ui| {
             ui.heading("Hello World!");
-            ui.indent("version", |ui|
+            ui.indent("version", |ui| {
                 ui.label(format!("Version: {}", RELEASE_VERSION))
-            );
-            
+            });
 
             // HACKIN
 
-            let buf = BufferRedirect::stdout().unwrap();
-            ListAll::from("sigi").run(OutputFormat::Tsv);
-            let mut rdr = csv::ReaderBuilder::new()
-                .delimiter(b'\t')
-                .from_reader(BufReader::new(buf.into_inner()));
+            let mut rdr = list_all("sigi");
 
             for result in rdr.records() {
                 let record = result.unwrap();
                 ui.label(record.get(1).unwrap());
             }
+
             // DONE HACKIN
         });
     }
 }
 
-pub fn run(){
+pub fn run() {
     let app = SigiApp::default();
     let native_options = epi::NativeOptions::default();
 
     eframe::run_native(Box::new(app), native_options);
+}
+
+type SigiReader = csv::Reader<BufReader<gag::Buffer>>;
+
+pub fn list_all(stack: &str) -> SigiReader {
+    captured(|| ListAll::from(stack).run(OutputFormat::Tsv))
+}
+
+pub fn captured<F>(tsv_printer: F) -> SigiReader
+where
+    F: FnOnce(),
+{
+    let buf = BufferRedirect::stdout().unwrap();
+
+    tsv_printer();
+
+    csv::ReaderBuilder::new()
+        .delimiter(b'\t')
+        .from_reader(BufReader::new(buf.into_inner()))
 }
